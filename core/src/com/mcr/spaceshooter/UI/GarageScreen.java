1468a673b13b93c3ff1124b2fd265641f899a587
GODOmegaPi <guillaume.laubscher@heig-vd.ch>
2021-06-17 13:53:49 +0200

    changed textures to match the game style

--- core/src/com/mcr/spaceshooter/UI/GarageScreen.java
+++ core/src/com/mcr/spaceshooter/UI/GarageScreen.java
@@ -11,6 +11,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.mcr.spaceshooter.Entity.Equipements.Equipment;
 import com.mcr.spaceshooter.Entity.Equipements.Fuselage;
@@ -37,38 +38,61 @@ public class GarageScreen implements Screen {
         skin = new Skin(Gdx.files.internal("skin/craftacular-ui.json"));
 
         // TODO voir comment opti new LinkedList<>([p1,p2,p3])
+        // TODO clean code
         fuselagesList = new LinkedList<>();
-        Pair p1 = new Pair<>(new Fuselage(10, 10), new Texture(Gdx.files.internal("ss_1.png")));
+        for (int i = 1; i <= 16; i++) {
+            Pair p = new Pair<>(new Fuselage(10, 10), new Texture(Gdx.files.internal("ships/cockpits/ship (" + i + ").png")));
+            fuselagesList.add(p);
+        }
+        /*Pair p1 = new Pair<>(new Fuselage(10, 10), new Texture(Gdx.files.internal("ss_1.png")));
         Pair p2 = new Pair<>(new Fuselage(10, 10), new Texture(Gdx.files.internal("ss_2.png")));
         Pair p3 = new Pair<>(new Fuselage(10, 10), new Texture(Gdx.files.internal("ss_3.png")));
 
         fuselagesList.add(p1);
         fuselagesList.add(p3);
-        fuselagesList.add(p2);
+        fuselagesList.add(p2);*/
 
         weaponsList = new LinkedList<>();
-        Pair pa = new Pair<>(new Weapon(10, 10), new Texture(Gdx.files.internal("badlogic.jpg")));
+        for (int i = 1; i <= 48; i++) {
+            Pair p = new Pair<>(new Weapon(10, 10), new Texture(Gdx.files.internal("ships/weapons/weapon (" + i + ").png")));
+            weaponsList.add(p);
+        }
+        /*Pair pa = new Pair<>(new Weapon(10, 10), new Texture(Gdx.files.internal("badlogic.jpg")));
         Pair pb = new Pair<>(new Weapon(10, 10), new Texture(Gdx.files.internal("game.png")));
         Pair pc = new Pair<>(new Weapon(10, 10), new Texture(Gdx.files.internal("noEquipement.jpg")));
         weaponsList.add(pc);
         weaponsList.add(pb);
-        weaponsList.add(pa);
+        weaponsList.add(pa);*/
 
 
         shieldsList = new LinkedList<>();
-        Pair px = new Pair<>(new Shield(10, 10), new Texture(Gdx.files.internal("badlogic.jpg")));
+        for (int i = 1; i <= 3; i++) {
+            Pair p = new Pair<>(new Shield(10, 10), new Texture(Gdx.files.internal("ships/shields/shield (" + i + ").png")));
+            shieldsList.add(p);
+        }
+        /*Pair px = new Pair<>(new Shield(10, 10), new Texture(Gdx.files.internal("badlogic.jpg")));
         Pair py = new Pair<>(new Shield(10, 10), new Texture(Gdx.files.internal("game.png")));
         Pair pz = new Pair<>(new Shield(10, 10), new Texture(Gdx.files.internal("noEquipement.jpg")));
         shieldsList.add(pz);
         shieldsList.add(px);
-        shieldsList.add(py);
+        shieldsList.add(py);*/
 
         Table table = new Table();
         table.setFillParent(true);
         stage.addActor(table);
 
         Label titleLabel = new Label("Garage", skin);
-        titleLabel.setFontScale(1);
+        titleLabel.setFontScale(3);
+
+        Label cockpitLabel = new Label("Cockpit", skin);
+        cockpitLabel.setFontScale(1);
+
+        Label weaponLabel = new Label("Weapon", skin);
+        cockpitLabel.setFontScale(1);
+
+        Label shieldLabel = new Label("Shield", skin);
+        cockpitLabel.setFontScale(1);
+
         TextButton playButton = new TextButton("Jouer", skin);
         playButton.addListener(new ClickListener(){
             @Override
@@ -89,11 +113,17 @@ public class GarageScreen implements Screen {
 
         table.add(titleLabel).colspan(3);
         table.row();
-        table.add(new ConfigRow( fuselagesList, skin)).height(200).colspan(3).center();
+        table.add(cockpitLabel).colspan(3);
         table.row();
-        table.add(new ConfigRow( fuselagesList, skin)).height(200).colspan(3).center();
+        table.add(new ConfigRow(fuselagesList, skin)).height(200).colspan(3).center();
         table.row();
-        table.add(new ConfigRow( fuselagesList, skin)).height(200).colspan(3).center();
+        table.add(weaponLabel).colspan(3);
+        table.row();
+        table.add(new ConfigRow(weaponsList, skin)).height(200).colspan(3).center();
+        table.row();
+        table.add(shieldLabel).colspan(3);
+        table.row();
+        table.add(new ConfigRow(shieldsList, skin)).height(200).colspan(3).center();
         table.row();
         table.add(playButton).width(300).colspan(3);
         table.row();
